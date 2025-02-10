bdd2cf6e603475e01982f9e3d99305442d7a4bfd
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2025-02-10 16:08:45 +0330

    component: [FIX] liked/unliked icon

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,9 @@ import React from "react";
 
 import logoFooter from "@/public/images/logo_footer.svg";
 
+import { MdOutlineLocationOn } from "react-icons/md";
+import { AiOutlinePhone } from "react-icons/ai";
+
 import Image from "next/image";
 
 export default function Footer() {
@@ -36,8 +39,14 @@ export default function Footer() {
         <div className="flex w-1/2 justify-end">
           <div className="w-2/3 flex flex-col space-y-2">
             <span className="font-bold">{t("contact-way")}</span>
-            <span>{t("address")}</span>
-            <span>{t("phone")}</span>
+            <span className="flex items-center"> 
+				<MdOutlineLocationOn size={24} className="me-1 text-primary"/>
+				{t("address")}
+			</span>
+            <span className="flex items-center">
+				<AiOutlinePhone size={24} className="me-1 text-primary"/>
+				{t("phone")}
+			</span>
             <div className="h-20 w-full flex justify-end">
               <Image src={logoFooter} alt={t("gashtook")} />
             </div>
