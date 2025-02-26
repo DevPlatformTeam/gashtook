26d567961e644afcd341e9b60fcb3159b80e355e
Kourosh Salmanzadeh <kouroshsalmanzadeh@gmail.com>
2025-02-26 21:42:09 +0330

    feat: [ADD] subscription-management Pages with No Data Handling and UI Improvements

--- src/app/[locale]/dashboard/favorites/page.tsx
+++ src/app/[locale]/dashboard/favorites/page.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from 'react';
+import Card from '@/components/Card/Card';
+import NoDataSvg from '@/icons/NoDataSvg';
+
+interface Card {
+  src: string;
+  label: string;
+  alt: string;
+  liked: boolean;
+}
+
+const cards: Card[] = [
+  {
+    src: "https://picsum.photos/id/201/300/200",
+    label: "Card Title 1",
+    alt: "Description for Card 1",
+    liked: false,
+  },
+  {
+    src: "https://picsum.photos/id/202/300/200",
+    label: "Card Title 2",
+    alt: "Description for Card 2",
+    liked: true,
+  },
+  {
+    src: "https://picsum.photos/id/203/300/200",
+    label: "Card Title 3",
+    alt: "Description for Card 3",
+    liked: false,
+  },
+];
 
 export default function Page() {
   return (
-    <div>علاقه مندی ها</div>
-  )
+    <div className="h-full text-right">
+      <h1 className="text-xl font-bold pb-2 mx-4 border-b border-gray-200">علاقه مندی ها</h1>
+      {cards.length > 0 ? <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 h-full overflow-y-auto scroll px-3 pt-4">
+        {cards.map((card, index) => (
+          <li key={index}>
+            <Card
+              src={card.src}
+              alt={card.alt}
+              label={card.label}
+              liked={card.liked}
+            />
+          </li>
+        ))}
+      </ul> :
+        <div className={"w-full h-full flex-center flex-col gap-4"}>
+          <NoDataSvg className="max-w-48 h-48" />
+          <p className="text-center text-gray-500">هیچ علاقه مندی یافت نشد</p>
+        </div>
+      }
+    </div>
+  );
 }
--- src/app/[locale]/dashboard/subscription-management/page.tsx
+++ src/app/[locale]/dashboard/subscription-management/page.tsx
@@ -1,7 +1,92 @@
+import Button from '@/components/Button/Button';
 import React from 'react'
+import { PiBriefcaseLight } from 'react-icons/pi';
+
+interface Subscription {
+  type: string;
+  price: string;
+  orderNumber: string;
+  status: boolean;
+  Referencecode: string;
+}
 
 export default function Page() {
+  const data: Subscription[] = [
+    {
+      type: "اشتراک سالانه",
+      price: "100,000 تومان",
+      orderNumber: "159",
+      status: true,
+      Referencecode: "1234567890",
+    },
+    {
+      type: "اشتراک روزانه",
+      price: "3,000 تومان",
+      orderNumber: "159",
+      status: true,
+      Referencecode: "1234567890",
+    },
+    {
+      type: "اشتراک روزانه",
+      price: "3,000 تومان",
+      orderNumber: "159",
+      status: false,
+      Referencecode: "1234567890",
+    },
+    {
+      type: "اشتراک روزانه",
+      price: "3,000 تومان",
+      orderNumber: "159",
+      status: false,
+      Referencecode: "1234567890",
+    },
+    {
+      type: "اشتراک روزانه",
+      price: "3,000 تومان",
+      orderNumber: "159",
+      status: true,
+      Referencecode: "1234567890",
+    },
+  ];
   return (
-    <div>مدیریت اشتراک</div>
-  )
+    <div className="h-full text-right">
+      <h1 className="text-xl font-bold pb-2 mx-4 border-b border-gray-200">مدیریت اشتراک</h1>
+      {data.length > 0 ? <table className="flex flex-col w-full h-full px-4 overflow-y-auto scroll pt-4">
+        <thead className='w-full flex justify-between items-center child:w-1/5 child:text-gray-400 pb-4 child:text-right border-b border-gray-100'>
+          <th>
+            <span>نوع اشتراک</span>
+          </th>
+          <th>
+            <span>قیمت</span>
+          </th>
+          <th>
+            <span>شماره سفارش</span>
+          </th>
+          <th>
+            <span>وضعیت</span>
+          </th>
+          <th>
+            <span>کد مرجع</span>
+          </th>
+        </thead>
+        <tbody className='w-full flex flex-col gap-3 divide-y divide-gray-100 child:pt-3'>
+          {data.map((card, index) => (
+            <tr key={index} className='w-full flex justify-between items-center child:w-1/4 child:text-secondary'>
+              <td>{card.type}</td>
+              <td>{card.price}</td>
+              <td>{card.orderNumber}</td>
+              <td>{card.status ? <span className='text-primary'>فعال</span> : <span className='text-red-400'>غیرفعال</span>}</td>
+              <td>{card.Referencecode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table> :
+        <div className={"w-full h-full flex-center flex-col"}>
+          <PiBriefcaseLight className="size-20 text-gray-500" />
+          <p className="text-center text-gray-500">شما تاکنون اشتراکی خریداری نکرده اید</p>
+          <Button text="خرید اشتراک" color='primary' textColor='background' className='font-normal mt-8 px-8' />
+        </div>
+      }
+    </div>
+  );
 }
