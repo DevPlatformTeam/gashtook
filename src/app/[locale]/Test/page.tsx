83f70e8c69bee5f816e7015d6cc76dae36ec5d0f
Ali Attar <m.aliattarr@gmail.com>
2025-03-04 19:02:00 +0330

    add map component and api key

--- src/app/[locale]/Test/page.tsx
+++ src/app/[locale]/Test/page.tsx
@@ -1,8 +1,12 @@
+"use client"
 import { useTranslations } from "next-intl";
 import { Link, routing } from "@/i18n/routing";
 import { FaCoffee } from "react-icons/fa";
 import Button from "@/components/Button/Button";
 import CheckBox from "@/components/CheckBox/CheckBox";
+import Map from "@/components/Map/Map";
+import Script from "next/script";
+import { useEffect, useState } from "react";
 
 import { notFound } from "next/navigation";
 // import SelectOptionComponent from "@/components/Select-option/SelectOption.component";
@@ -14,6 +18,15 @@ export default function HomePage({
   params: { locale: string };
 }) {
   const t = useTranslations("HomePage");
+  const locations = [
+    { lat: 35.6892, lng: 51.389 }, // تهران
+    { lat: 36.2605, lng: 59.6168 }, // مشهد
+    { lat: 29.5914, lng: 52.5837 }, // شیراز
+  ];
+
+  const [loaded, setLoaded] = useState(false);
+
+
 
   if (!routing.locales.includes(locale as (typeof routing.locales)[number])) {
     return notFound();
@@ -52,6 +65,9 @@ export default function HomePage({
           id="select-option"
         />
         <SearchInputComponents id="search" placeholder="سرچ کنید...." /> */}
+        <Map locations={locations} />
+
+
       </div>
     </div>
   );
