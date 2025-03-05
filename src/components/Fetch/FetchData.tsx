c6ef50237d70aeb61d17750bfedf5dde0ddf9397
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2025-03-05 11:44:40 +0330

    refactor: Update useFetchData hook to support dynamic API endpoint

--- src/components/Fetch/FetchData.tsx
+++ src/components/Fetch/FetchData.tsx
@@ -2,14 +2,14 @@
 
 import { useState } from "react";
 
-export const useFetchData = () => {
+export const useFetchData = (endpoint: string) => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<{isError: boolean; message: string}>({isError: false, message: ""});
     const [data, setData] = useState<Array<{ id: number; title: string; body: string }>>([]);
 
     try {
         setIsLoading(true);
-        fetch("https://jsonplaceholder.typicode.com/posts")
+        fetch(`${process.env.NEXT_PUBLIC_BASE_URL_API}/${endpoint}`)
         .then((res) => res.json())
         .then((json) => setData(json));
         setError({isError: false, message: ""});
