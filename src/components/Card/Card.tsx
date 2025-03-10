29ee6080e96a8654d46068eabf22e5286a109cc8
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2025-03-10 14:53:26 +0330

    enhance: set image loader

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
-
 import { IoHeart } from "react-icons/io5";
 import { IoMdHeartEmpty } from "react-icons/io";
-
 import Image from "next/image";
 
 interface ICardProps {
@@ -12,45 +10,42 @@ interface ICardProps {
   label: string;
   alt: string;
   liked?: boolean;
+  showLikedBtn?: boolean;
 }
 
-export default function Card({ src, label, alt, liked = false }: ICardProps) {
-  const [hasError, setHasError] = useState(false);
+export default function Card({
+  src,
+  label,
+  alt,
+  liked = false,
+  showLikedBtn = true,
+}: ICardProps) {
   const [isLoading, setIsLoading] = useState(true);
-
-  
-  if (isLoading) {
-    return (
-      <>
-        <div className="min-h-40 relative rounded-lg animate-pulse bg-slate-200 child:animate-pulse">
-          <button
-            className="bg-slate-100 text-slate-300 shadow-lg flex justify-center items-center rounded-full w-9 h-9 absolute end-4 bottom-5"
-            type="button"
-          >
-          </button>
-        </div>
-        <div className="w-30 h-8 bg-slate-200 rounded-lg ps-0.5 z-1 animate-pulse mt-2"></div>
-      </>
-    )
-  }
 
   return (
     <div className="min-h-40 relative flex flex-col gap-2">
-      <Image
-        className="!relative rounded-lg"
-        fill
-        src={src}
-        alt={alt}
-        onLoad={() => setIsLoading(false)}
-        onError={() => setHasError(true)}
-      />
-      {hasError && <div>Error loading image</div>}
-      <button
-        className="bg-primary shadow-lg flex justify-center items-center text-white rounded-full w-9 h-9 absolute end-3 bottom-11"
-        type="button"
-      >
-        {liked ? <IoHeart size={22} /> : <IoMdHeartEmpty size={22} />}
-      </button>
+      <div className="relative">
+        {isLoading && (
+          <div className="absolute inset-0 bg-gray-300 animate-pulse rounded-lg"></div>
+        )}
+        <Image
+          className={`rounded-lg !relative drop-shadow-xl shadow-lg !w-96 !h-auto object-cover transition-opacity duration-300 ${
+            isLoading ? "opacity-0" : "opacity-100"
+          }`}
+          src={src}
+          alt={alt}
+          fill
+          onLoad={() => setIsLoading(false)}
+        />
+      </div>
+      {showLikedBtn && (
+        <button
+          className="bg-primary shadow-lg flex justify-center items-center text-white rounded-full w-9 h-9 absolute end-3 bottom-11"
+          type="button"
+        >
+          {liked ? <IoHeart size={22} /> : <IoMdHeartEmpty size={22} />}
+        </button>
+      )}
       <p className="ps-0.5 text-start">{label}</p>
     </div>
   );
